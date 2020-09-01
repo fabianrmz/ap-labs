45244e16c94ddf610bb8c5844eb18c49244af0ed
Óscar Fabián Ramírez González <A01630024@itesm.mx>
2020-08-31 20:38:06 -0500

    Solve word-reverser

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -2,5 +2,21 @@
 
 int main(){
     // Place your magic here
+     char c;
+    char word[50];
+    int count = 0;
+    while((c = getchar()) != EOF){
+        if(c=='\n') {
+            count--;
+            while(count>=0) {
+                printf("%c", word[count--]);
+            }
+            count = 0;
+            printf("\n");
+        }
+        else {
+            word[count++]=c;
+        }
+    }    
     return 0;
 }
