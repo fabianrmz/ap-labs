aab482f10b36795ace21aec1ca0669cd9bb1fd51
Óscar Fabián Ramírez González <A01630024@itesm.mx>
2020-09-08 13:30:39 -0500

    Solve str-lib

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -3,11 +3,16 @@ char newString[150];
 int main(int argc, char *argv[])
 {
 
-    if (argc == 4)
+    if (argc >= 2)
     {
-        
         printf("Initial Lenght      : %i\n", mystrlen(argv[1]));
+    }
+    if (argc >= 3)
+    {
         mystradd(argv[1], argv[2]);
+    }
+    if (argc >= 4)
+    {
         if(mystrfind(newString, argv[3])!= -1){
             printf("Substring was found:      : yes\n");
         }else{
