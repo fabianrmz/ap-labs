04e3edeffd5ceacaf411d4498ae055732ad9d92e
Óscar Fabián Ramírez González <A01630024@itesm.mx>
2020-09-19 14:38:18 -0500

    Solve go-slices-maps
    Signed-off-by: Óscar Fabián Ramírez González <A01630024@itesm.mx>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	counter := make(map[string]int)
+	words := strings.Fields(s)
+    for _ ,word:= range words{
+        counter[word]++
+    }
+    return counter
+}
+
 func main() {
+	wc.Test(WordCount)
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,19 @@
 package main
 
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+    var total[][]uint8
+    total = make([][]uint8, dy)
+    for k := range total {
+        total[k] = make([]uint8, dx)
+        for i := range total[k] {
+            total[k][i] = uint8((i + k) /2)
+        }
+    }
+    return total
+}
+
 func main() {
+	pic.Show(Pic)
 }
