39356acb68509ef1eeede38dc2787d997b72e518
Óscar Fabián Ramírez González <A01630024@itesm.mx>
2020-09-25 17:23:02 -0500

    Solve advanced-logger
    Signed-off-by: Óscar Fabián Ramírez González <A01630024@itesm.mx>

---
+++ no-free.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+int main() {
+    char *a = NULL;
+    char *b = NULL;
+
+    a = foo("Hi there, Chris");
+    free(a);
+    b = foo("Goodbye");
+    free(b);
+    printf("From main: %s %s\n", a, b);
+    return 0;
+}
+
+char *foo(char *p) {
+    char *q = (char *)malloc(strlen(p)+1);
+    strcpy(q, p);
+    printf("From foo: the string is %s\n", q);
+    return q;
+}
+
